573bd56931878741a4b723e44806aae4a8105afc
Your Name <tpunchihewage@gmail.com>
2022-07-05 14:57:12 +0200

    added Data to solutions file

---
+++ solution.js
@@ -0,0 +1,20 @@
+// Exercise - 01
+
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+
+console.log(`${introSentence} ${age}`);
+
+// The + operator performs concatenation operation when one of the operands is of string type.
+
+// Exercise - 02 : i
+
+const integer = "1005";
+
+console.log(parseInt(integer));
+
+// Exercise - 02 : ii
+
+const float = "10.05";
+
+console.log(parseFloat(float));
